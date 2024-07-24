51388a1647c1cb578c30e6f87b87a7acb138b6f0
unknown <Mohdinho10>
2024-07-24 15:49:04 +0300

    updated some changes in app.js

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,6 @@ app.use(
 );
 
 if (process.env.NODE_ENV === "production") {
-  const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) =>
