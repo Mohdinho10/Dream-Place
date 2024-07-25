697a5232158219e5dcbfda7d8db03e14cd560ec7
unknown <Mohdinho10>
2024-07-25 11:21:40 +0300

    implemented lazy loading in app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,19 +2,29 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ToastContainer } from "react-toastify";
+import { UserProvider } from "./context/UserContext";
+import { SocketProvider } from "./context/SocketContext";
 import "react-toastify/dist/ReactToastify.css";
 import AppLayout from "./components/AppLayout";
-import HomePage from "./pages/HomePage";
-import ListingsPage from "./pages/ListingsPage";
-import ListingPage from "./pages/ListingPage";
-import ProfilePage from "./pages/ProfilePage";
-import LoginPage from "./pages/LoginPage";
-import RegisterPage from "./pages/RegisterPage";
-import { UserProvider } from "./context/UserContext";
 import ProtectedRoutes from "./components/ProtectedRoutes";
-import ProfileUpdatePage from "./pages/ProfileUpdatePage";
-import NewPostPage from "./pages/NewPostPage";
-import { SocketProvider } from "./context/SocketContext";
+import { lazy } from "react";
+const HomePage = lazy(() => import("./pages/HomePage"));
+const ListingsPage = lazy(() => import("./pages/ListingsPage"));
+const ListingPage = lazy(() => import("./pages/ListingPage"));
+const ProfilePage = lazy(() => import("./pages/ProfilePage"));
+const LoginPage = lazy(() => import("./pages/LoginPage"));
+const RegisterPage = lazy(() => import("./pages/RegisterPage"));
+const ProfileUpdatePage = lazy(() => import("./pages/ProfileUpdatePage"));
+const NewPostPage = lazy(() => import("./pages/NewPostPage"));
+
+// import HomePage from "./pages/HomePage";
+// import ListingsPage from "./pages/ListingsPage";
+// import ListingPage from "./pages/ListingPage";
+// import ProfilePage from "./pages/ProfilePage";
+// import LoginPage from "./pages/LoginPage";
+// import RegisterPage from "./pages/RegisterPage";
+// import ProfileUpdatePage from "./pages/ProfileUpdatePage";
+// import NewPostPage from "./pages/NewPostPage";
 
 const queryClient = new QueryClient({
   defaultOptions: {
